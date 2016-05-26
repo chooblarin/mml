96dddcde0e92b3568abf7ac0bbf1ba645dc4fd33
chooblarin <choo.bla.rin@gmail.com>
2016-05-26 09:37:52 +0900

    Add some comments

--- js/scripts/Network.js
+++ js/scripts/Network.js
@@ -3,9 +3,20 @@ const Util = require('./Util.js');
 
 class Network {
 
+  /**
+    * @param sizes : the array contains the number of neurons in the respective layers.
+    * For example, if we want to create a Network instance with
+    * 2 neurons in the first layer,
+    * 3 neurons in the second layer,
+    * and 1 neuron in the last layer, we would give [2, 3, 1] to the constructor.
+    */
   constructor(sizes) {
     this.sizes = sizes;
     this.numOfLayers = sizes.length;
+    /*
+      biases and weights are all initialized randomly, using a Gaussian distributions
+      with mean 0 and standard deviation 1.
+    */
     this.biases = sizes
           .slice(1, sizes.length).map(n => Math.random());
     this.weights = _.zip(sizes, sizes)
