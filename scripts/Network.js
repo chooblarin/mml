b39bdbd991d1e53aa6b51ff047f42bfd0c32c148
chooblarin <choo.bla.rin@gmail.com>
2016-05-26 09:11:26 +0900

    Define methods to Network class

--- scripts/Network.js
+++ scripts/Network.js
@@ -6,7 +6,21 @@ class Network {
   constructor(sizes) {
     this.sizes = sizes;
     this.numOfLayers = sizes.length;
-    this.biases = sizes.slice(1, sizes.length).map(n => Math.random());
-    // this.weights = 
+    this.biases = sizes
+          .slice(1, sizes.length).map(n => Math.random());
+    this.weights = _.zip(sizes, sizes)
+          .map(pair => Util.randomMatrix(pair[1], pair[0]));
+  }
+
+  feedforward(input) {
+  }
+
+  stochasticGradientDecscent() {
+  }
+
+  updateMiniBatch(miniBatch, eta) {
+  }
+
+  backPropagation(x, y) {
   }
 }
